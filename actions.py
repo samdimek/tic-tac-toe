67bae50369ee9b90e0e2cc497841e9824775732c
samdimek <delashoo.uk@gmail.com>
2024-03-04 02:27:00 +0300

    Added 3 files.

---
+++ actions.py
@@ -0,0 +1,1 @@
+# Simple class to specify actions to be made on the T3 board state, specifying a column, row, and number to place.
---
+++ state.py
@@ -0,0 +1,7 @@
+
+"""
+All logic concerning the T3 board state, including:
+    1. the ability to get the transitions(which you must complete!)
+    2. and available actions,
+    3. and determine if the turn is for the odd / even player
+"""
